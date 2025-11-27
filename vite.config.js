51d8f7c4e79f5eef90e1dc22781e5ee9d2ce5eae
Andile <andile7117@gmail.com>
2025-11-27 20:39:30 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: '/GreenScape/', // 👈 must match your GitHub repo name
+});
+
